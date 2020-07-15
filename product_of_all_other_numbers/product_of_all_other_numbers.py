f8d77ed5b9c92c78ea510264f6ee1450e95e9be9
vanesa-tamu <vahe5495@tamu.edu>
2020-07-14 23:05:15 -0500

    complete product_of_all_other_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,32 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
-
+    new_arr = []
+    for i in range(0, len(arr)):
+        print(f'index i is at: {i}')
+        # save index's value (old)
+        old_value_i = arr[i]
+        # change value to: 1
+        arr[i] = 1
+        # multiply all nums in arr and traverse
+        result = 1
+        for j in range(0, len(arr)):
+            print(f'what is j value? {j} value is {arr[j]}')
+            result = result * arr[j]
+            print(f'result in j loop: {result}')
+        new_arr.append(result)  # append result of j after loop
+        # change value back to old value:
+        arr[i] = old_value_i
+    # return new array
+    return new_arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr1 = [1, 2, 3, 4, 5]  # [120, 60, 40, 30, 24]
+    arr2 = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr1)}")
